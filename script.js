158705b5f1a6d80842f99d0bec519379a87b18e2
Laurie Lespinasse <lespinasse.laurie5@gmail.com>
2025-11-10 12:07:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const filepath = "data.json";
+
+const content = fs.readFileSync(filepath, { encoding: "utf8", flag: "r" });
+const data = JSON.parse(content);
+console.log(data);

9343b9d89fccc9edfe05a3a0837c56707cabc011
Laurie Lespinasse <lespinasse.laurie5@gmail.com>
2025-11-10 14:23:40 +0100

    Etape 3

--- script.js
+++ script.js
@@ -10,3 +10,7 @@ console.log(typeof data);
 // Etape 2 : Affiche dans la console, le premier objet du tableau d’objets
 const firstObject = data[0];
 console.log(firstObject);
+
+// Etape 3 : Afficher dans la console le nombre d’objets contenu dans le tableau
+const dataLength = data.length;
+console.log(dataLength);
